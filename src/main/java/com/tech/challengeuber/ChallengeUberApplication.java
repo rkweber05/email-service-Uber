c4c8b79972cb972966c79dd765b4266b3a877cea
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-08-27 16:51:06 -0300

    Fisrt commit: Email Service

---
+++ src/main/java/com/tech/challengeuber/ChallengeUberApplication.java
@@ -0,0 +1,13 @@
+package com.tech.challengeuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeUberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeUberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/challengeuber/ChallengeUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.challengeuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeUberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
